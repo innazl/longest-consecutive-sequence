8d7ba17e59e47f6b9644308f6839b9d14d8d80f1
innazl <mozyr.innazl@tut.by>
2018-10-07 09:03:28 +0300

    longest-consecutive-sequence: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  const sequence = {};
+  let count = 1;
+  let arlength = array.length;
+  for (let i = 0; i < arlength; i++) {
+    const currelem = array[i];
+     if (sequence.hasOwnProperty(currelem)) {
+      continue;
+    }
+    sequence[currelem] = 1;
+    if (sequence.hasOwnProperty(currelem - 1)) {
+      count = Math.max(count, merge(sequence, currelem - 1, currelem));
+    }
+    if (sequence.hasOwnProperty(currelem + 1)) {
+      count = Math.max(count, merge(sequence, currelem, currelem + 1));
+    }
+  }
+  if (!arlength) count = 0;
+  return count;
+}
+function merge(sequence, left, right) {
+  const prev = right + sequence[right] - 1;
+  const next = left - sequence[left] + 1;
+  const len = prev - next + 1;
+  sequence[prev] = len;
+  sequence[next] = len;
+  return len;
 }
